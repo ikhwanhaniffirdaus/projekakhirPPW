5d440a47cee8c58c73aed68bf9a8c0f6d94be467
ikhwanhaniffirdaus <137202727+ikhwanhaniffirdaus@users.noreply.github.com>
2023-06-20 23:36:27 +0700

    Add files via upload

---
+++ projek akhir web PPW/Produk.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+      crossorigin="anonymous"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Mukta:wght@200&family=Open+Sans:wght@300&family=Poppins:wght@100;300;400;700&family=Roboto+Slab:wght@300;500&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Mukta:wght@200&family=Open+Sans:wght@300&family=Poppins:wght@100;300;400;700&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="produk_css.css" />
+  </head>
+
+  <body>
+    <section id="banner_img">
+      <div class="teks_banner">
+        <h2>ADA RUBIK</h2>
+        <h2>APA AJA SIH</h2>
+      </div>
+    </section>
+    <svg viewBox="0 0 500 100">
+      <path d="M 0,10 C 150,80 350,0 500,10 L 500,00 L 0,00"></path>
+    </svg>
+
+    <!-- KONTEN -->
+    <section id="konten1">
+      <h1>BERIKUT BEBERAPA JENIS RUBIK TERBAIK</h1>
+      <div class="produk-card-kiri">
+        <div class="img1"></div>
+        <div class="deskripsi-kiri">
+          <h1>GAN 13 M</h1>
+          <p>
+            GAN 13 3x3 Magnetic (MagLev) adalah masa depan speedcubing. 13
+            menawarkan banyak fitur baru dan peningkatan yang pasti tingkatkan
+            pengalaman pemecahan Anda!
+          </p>
+          <h2>Fitur</h2>
+          <ul>
+            <li>Corner/core magnets (Flagship & Pioneer versions)</li>
+            <li>Easy-to-use adjustment system</li>
+            <li>Magnetic repulsion (MagLev) technology (Pioneer version)</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="produk-card-kiri">
+        <div class="img2"></div>
+        <div class="deskripsi-kiri">
+          <h1>MOYU RS3M</h1>
+          <p>
+            MoYu RS3 M 2020 3x3 Magnetic bisa dibilang merupakan yang terbaik
+            untuk uang Anda dan tidak mengherankan jika ini adalah kubus
+            kecepatan terlaris kami. FYI, rubik ini adalah salah satu rubik
+            sejuta umat.
+          </p>
+          <h2>Fitur</h2>
+          <ul>
+            <li>Spring compression system (9 settings)</li>
+            <li>
+              Frosted exterior for improved grip (Stickerless (Bright) only)
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="produk-card-kiri">
+        <div class="img3"></div>
+        <div class="deskripsi-kiri">
+          <h1>X-MAN TORNADO V3M</h1>
+          <p>
+            X-Man Tornado V3 3x3 Magnetic menghadirkan fitur baru di tiga versi
+            berbeda! Rubik ini dikenal karena kecepatan putaran dan suaranya
+            yang tenang
+          </p>
+          <h2>Fitur</h2>
+          <ul>
+            <li>Corner/core magnets (Flagship & Pioneer versions)</li>
+            <li>Easy-to-use adjustment system</li>
+            <li>Magnetic repulsion (MagLev) technology (Pioneer version)</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="produk-card-kiri">
+        <div class="img4"></div>
+        <div class="deskripsi-kiri">
+          <h1>DAYAN TENGYUN V3M</h1>
+          <p>
+            DaYan TengYun V3 3x3 Magnetic adalah pembaruan utama untuk seri
+            TengYun!, Rubik ini terkenal karena keheningan pputaranya
+          </p>
+          <h2>Fitur</h2>
+          <ul>
+            <li>Adjustable magnet strength (5 settings)</li>
+            <li>
+              Dual-adjustment core for independent spring compression & tension
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+
+    <section id="footer">
+      <div id="list-gambar">
+        <div class="medsos">
+          <img src="bahanwebsite/facebook.svg" alt="" />
+        </div>
+        <div class="medsos">
+          <img src="bahanwebsite/instagram.svg" alt="" />
+        </div>
+        <div class="medsos">
+          <img src="bahanwebsite/twitter.svg" alt="" />
+        </div>
+      </div>
+      <p>Copyright. ALL rights reserved</p>
+    </section>
+  </body>
+</html>
---
+++ projek akhir web PPW/world_record_3x3.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+      crossorigin="anonymous"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Mukta:wght@200&family=Open+Sans:wght@300&family=Poppins:wght@100;300;400;700&family=Roboto+Slab:wght@300;500&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Mukta:wght@200&family=Open+Sans:wght@300&family=Poppins:wght@100;300;400;700&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="wr_css.css" />
+  </head>
+  <body>
+    <div class="header">
+      <h1>WORLD RANKS</h1>
+      <p>berikut data ranking di beberapa kategori</p>
+      <div id="list-kategori">
+      <div id="kat1">
+          <a href="http://localhost/projek akhir web PPW/world_record_2x2.php"><img src="bahanwebsite/2x2.png" alt="" /></a>
+        </div>
+        <div id="kat2">
+          <a href="http://localhost/projek akhir web PPW/world_record_3x3.php"><img src="bahanwebsite/3x3.png" alt="" /></a>
+        </div>
+        <div id="kat3">
+          <a href="http://localhost/projek akhir web PPW/world_record_4x4.php"><img src="bahanwebsite/3x3.png" /></a>
+        </div>
+        <div id="kat4">
+          <a href="http://localhost/projek akhir web PPW/world_recordpyraminx.php"><img src="bahanwebsite/pyraminx.png" /></a>
+        </div>
+      </div>
+    </div>
+
+    <div class="tabel">
+      <table class="table table-striped">
+        <h1>3x3 Worlds Rank</h1>
+        <thead>
+          <tr>
+            <th scope="col">Rank</th>
+            <th scope="col">Name</th>
+            <th scope="col">Average</th>
+            <th scope="col">Country</th>
+            <th scope="col">Competition</th>
+            <th scope="col">Result1</th>
+            <th scope="col">Result2</th>
+            <th scope="col">Result3</th>
+            <th scope="col">Result4</th>
+            <th scope="col">Result5</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          include'koneksi_database.php';
+          $mahasiswa = mysqli_query($conn, "select * from cubers");
+          $no = 1;
+          echo"<br>
+          <br />"; foreach ($mahasiswa as $value){ echo"
+          <tr>
+            <td>" . $value['Ranking']. "</td>
+            <td>" . $value['Name'] . "</td>
+            <td>" . $value['Average']. "</td>
+            <td>" . $value['Country']. "</td>
+            <td>" . $value['Competition'] . "</td>
+            <td>" . $value['Result1']. "</td>
+            <td>" . $value['Result2']. "</td>
+            <td>" . $value['Result3'] . "</td>
+            <td>" . $value['Result4']. "</td>
+            <td>" . $value['Result5']. "</td>
+          </tr>
+          "; } ?>
+        </tbody>
+      </table>
+    </div>
+
+    <section id="footer">
+        <div id="list-gambar">
+          <div class="medsos">
+            <img src="bahanwebsite/facebook_b.svg" alt="" />
+          </div>
+          <div class="medsos">
+            <img src="bahanwebsite/instagram_b.svg" alt="" />
+          </div>
+          <div class="medsos">
+            <img src="bahanwebsite/twitter_b.svg" alt="" />
+          </div>
+        </div>
+        <p>Copyright. ALL rights reserved</p>
+      </section>
+  </body>
+</html>
